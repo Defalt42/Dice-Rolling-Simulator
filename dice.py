bf5618f25bb11aaf6f1f0db902b0e5c758b875c4
Bryan Gilbert <gilbertb4375@gmail.com>
2018-12-21 12:49:15 -0600

    Add random generator and loop

--- dice.py
+++ dice.py
@@ -1,1 +1,16 @@
-print("Hello, World!")
+import random
+# Random number between 1 and 6 inclusive
+
+while True:
+	# Ask user if they would like to roll
+	choice = input("Would you like to roll? (yes or no)\n")
+
+	if choice.strip().lower() == 'yes':
+		rand_num = random.randint(1, 6)
+		# print number on die
+		print(rand_num)
+	elif choice.strip().lower() == 'no':
+		print("Thanks for rolling!")
+		break
+	else:
+		print("I am not sure I understand what you are asking. Try again.")

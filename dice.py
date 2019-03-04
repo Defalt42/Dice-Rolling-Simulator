2cbdb4e126d5b3e0cec355ee04d04476a94b9959
Bryan Gilbert <gilbertb4375@gmail.com>
2019-03-04 16:21:19 -0600

    Add roll() function and ability to roll multiple dice

--- dice.py
+++ dice.py
@@ -1,14 +1,22 @@
 import random
-# Random number between 1 and 6 inclusive
+
+# Returns a random number between 1 and 6
+def roll(num_dice):
+	dice_nums = [random.randint(1,6) for x in range(num_dice)]
+
+	return dice_nums
 
 while True:
 	# Ask user if they would like to roll
 	choice = input("Would you like to roll? (yes or no)\n")
 
 	if choice.strip().lower() == 'yes':
-		rand_num = random.randint(1, 6)
+		num_dice = int(input("How many dice would you like to roll?\n"))
+		# Roll dice
+		dice = roll(num_dice)
 		# print number on die
-		print(rand_num)
+		for die in dice:
+			print("You rolled a " + str(die))
 	elif choice.strip().lower() == 'no':
 		print("Thanks for rolling!")
 		break
